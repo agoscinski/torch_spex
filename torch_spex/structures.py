df8db3b82dafee31809df6597509bd3e1746af03
Alexander Goscinski <alex.goscinski@posteo.de>
2023-06-04 13:40:02 +0200

    Remove ase.Atoms dependence of SphericalExpansion
    * replace Structure class with a Dict[str, torch.Tensor] because torch
      does not support non torch Tensor types for tracing
    * the conversion from ase.Atoms to Dict[str, torch.Tensor]  is done within a function
       because TorchScript does not support foreign types
    * add typing for various inputs

--- torch_spex/structures.py
+++ torch_spex/structures.py
@@ -1,37 +1,41 @@
 import numpy as np
 import torch
+from typing import Dict, List
+import ase
 
+def ase_atoms_to_tensordict(atoms_list : List[ase.Atoms]) -> Dict[str, torch.Tensor]:
+    """
+    dictionary contains
+    - **n_structures**: ...,
+    - **positions**: ...,
+    - **cells**: ...,
+    - **structure_indices**: ...,
+    - **atomic_species**: ...
+    - **cells**: ...,
+    - **structure_indices**: ...,
+    - **atomic_species**: ...,
+    - **pbc**: ...
+    """
 
-class Structures:
+    positions = []
+    cells = []
+    structure_indices = []
+    atomic_species = []
+    pbcs = []
 
-    # This class essentially takes a list of Atoms objects and converts
-    # all the relevant data into torch data structures
+    for structure_index, atoms in enumerate(atoms_list):
+        positions.append(atoms.positions)
+        cells.append(atoms.cell)
+        for _ in range(atoms.positions.shape[0]):
+            structure_indices.append(structure_index)
+        atomic_species.append(atoms.get_atomic_numbers())
+        pbcs.append(atoms.pbc)
 
-    def __init__(self, atoms_list) -> None:
-        
-        self.n_structures = len(atoms_list)
-
-        positions = []
-        cells = []
-        structure_indices = []
-        atomic_species = []
-        pbcs = []
-
-        for structure_index, atoms in enumerate(atoms_list):
-            positions.append(atoms.positions)
-            cells.append(atoms.cell)
-            for _ in range(atoms.positions.shape[0]):
-                structure_indices.append(structure_index)
-            atomic_species.append(atoms.get_atomic_numbers())
-            pbcs.append(atoms.pbc)
-
-        self.positions = torch.tensor(np.concatenate(positions, axis=0), dtype=torch.get_default_dtype())
-        self.cells = cells
-        self.structure_indices = np.array(structure_indices)
-        self.atomic_species = atomic_species
-        self.pbcs = pbcs
-
-    def to(self, device):
-
-        self.positions = self.positions.to(device)
-
+    atomic_structures = {}
+    atomic_structures["n_structures"] = torch.tensor(len(atoms_list))
+    atomic_structures["positions"] = torch.tensor(np.concatenate(positions, axis=0), dtype=torch.get_default_dtype())
+    atomic_structures["cells"] = torch.tensor(cells)
+    atomic_structures["structure_indices"] = torch.tensor(structure_indices)
+    atomic_structures["atomic_species"] = torch.tensor(atomic_species)
+    atomic_structures["pbcs"] = torch.tensor(pbcs)
+    return atomic_structures
